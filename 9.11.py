6526e34ba2303459b6513f29caf963a12093d34d
Leo Elmecker-Plakolm <leo.elmecker-plakolm@student.manchester.ac.uk>
2021-08-26 23:47:06 +0200

    9.11

---
+++ 9.11.py
@@ -0,0 +1,42 @@
+class BinaryTree():
+    def __init__(self, val = None, right = None, left = None, parent = None):
+        self.val = val
+        self.right = right
+        self.left = left
+        self.parent = parent
+
+
+t = BinaryTree(val = 1)
+t.left = BinaryTree(val = 2, parent = t)
+t.right = BinaryTree(val = 2, parent = t)
+t.left.right = BinaryTree(val = 3, parent=t.left)
+t.left.left = BinaryTree(val = 4, parent=t.left)
+t.right.left = BinaryTree(val = 3, parent=t.right)
+t.right.right = BinaryTree(val = 4, parent=t.right)
+
+def inorder(t):
+    prev, result = None, []
+
+    while t:
+        next = None
+        if prev == t.parent:
+            if t.left:
+                next = t.left
+            else:
+                result.append(t.val)
+
+                next = t.right or t.parent
+
+        elif t.left is prev:
+            result.append(t.val)
+
+            next = t.right or t.parent
+        
+        else:
+            next = t.parent
+
+        prev, t = t, next
+
+    return result
+
+print(inorder(t))
